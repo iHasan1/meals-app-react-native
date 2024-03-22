9aedf092ba539b0c8be1f4361fa0305df91ff565
Imran Hasan <imran.h94@gmail.com>
2024-03-22 07:17:07 +0500

    added styling for screen headers and backgounds though react navigation

--- App.js
+++ App.js
@@ -13,9 +13,28 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MealsCategories">
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        <Stack.Navigator
+          initialRouteName="MealsCategories"
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{
+              title: "All Meals",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
